9c95008a3111048205418d4f9815ef2cceaf2455
Richard Pablo <richardpablop@gmail.com>
2017-10-24 14:38:04 +0100

    First commit

---
+++ src/main/java/com/r1ckr/App.java
@@ -0,0 +1,26 @@
+package com.r1ckr;
+
+import io.swagger.client.ApiException;
+import io.swagger.client.ApiResponse;
+import io.swagger.client.api.QuotesApi;
+import io.swagger.client.model.QueryResponse;
+import io.swagger.client.model.Quote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws ApiException {
+        QuotesApi quotesApi = new QuotesApi();
+        ApiResponse<QueryResponse> response= quotesApi.publicYqlGetWithHttpInfo(
+                "select * from yahoo.finance.quote where symbol in (\"USDEUR=X\",\"USDGBP=X\")",
+                "json",
+                "store://datatables.org/alltableswithkeys", null);
+
+        for (Quote quote: response.getData().getQuery().getResults().getQuote()) {
+            System.out.println(quote.getSymbol() +" = "+ quote.getLastTradePriceOnly());
+        }
+    }
+}
