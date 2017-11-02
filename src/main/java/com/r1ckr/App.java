a62f178f808c741def203ec7a72986351abdf20e
Richard Pablo <richardpablop@gmail.com>
2017-11-02 21:29:24 +0000

    Setting operationId for the API call

--- src/main/java/com/r1ckr/App.java
+++ src/main/java/com/r1ckr/App.java
@@ -10,13 +10,14 @@ public class App
 {
     public static void main( String[] args ) throws ApiException {
         QuotesApi quotesApi = new QuotesApi();
-        ApiResponse<QueryResponse> response= quotesApi.publicYqlGetWithHttpInfo(
-                "select * from yahoo.finance.quote where symbol in (\"USDEUR=X\",\"USDGBP=X\")",
+        ApiResponse<QueryResponse> response= quotesApi.executeYqlWithHttpInfo(
+                "select * from yahoo.finance.quote where symbol in (\"GBPEUR=X\",\"USDAFN=X\")",
                 "json",
                 "store://datatables.org/alltableswithkeys", null);
 
         for (Quote quote: response.getData().getQuery().getResults().getQuote()) {
             System.out.println(quote.getSymbol() +" = "+ quote.getLastTradePriceOnly());
         }
+        System.out.println("Done");
     }
 }
